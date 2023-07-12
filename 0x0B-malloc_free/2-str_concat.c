fad918a8a5ed12166faac8d948daf55d0a3bf497
Joseph-Oje <josephoje247@gmail.com>
2023-07-11 21:21:27 -0700

    compulsry task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,8 +19,9 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 == NULL)
 		s2 = "";
-	while (s1 == NULL)
-		s1 = "";
+
+	while (s1[i])
+		i++;
 
 	while (s2[j])
 		j++;
@@ -31,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 	if (s == NULL)
 		return (NULL);
 	j = 0;
-	while (k < i)
+	while (k < l)
 	{
 		if (k <= i)
 			s[k] = s1[k];
