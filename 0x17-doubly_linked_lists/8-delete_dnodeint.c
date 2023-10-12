cbe25ecdb9d66eb9b6467ad096ad9889552cce59
Joseph-Oje <josephoje247@gmail.com>
2023-10-12 16:53:21 -0700

    my doubly linked lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,42 +1,46 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes node at specific position
- *
- * Description:
- * @head: double pointer to head node
- * @index: position of node to be deleted
- *
- * Return: 1 if successful, otherwise fail
-*/
-
+ * delete_dnodeint_at_index - deletes the node at index
+ * @head: head of list
+ * @index: index of list
+ * Return: 1 if it succeeded, -1 if it failed
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp;
-	unsigned int count = 0;
+	dlistint_t *aux = NULL;
+	int idx = index, i;
 
-	if (!*head)
+	if (head == NULL || *head == NULL || dlistint_len(*head) < index)
 		return (-1);
 
-	temp = *head;
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(temp);
-		if (*head)
-			(*head)->prev = NULL;
-		return (1);
-	}
-	while (count < index)
-	{
-		count++;
-		temp = temp->next;
+	aux = *head;
+	for (i = 0; i < idx; i++)
+		aux = aux->next;
 
-		if (!temp) /* when idx is out of range */
-			return (-1);
-	}
-	temp->prev->next = temp->next;
-	temp->next->prev = temp->prev;
-	free(temp);
+	if (aux->prev != NULL)
+		aux->prev->next = aux->next;
+	else if (aux->next != NULL)
+		*head = aux->next;
+	else
+		*head = NULL;
+	if (aux->next != NULL)
+		aux->next->prev = aux->prev;
+
+	free(aux);
 	return (1);
+}
+
+/**
+ *  dlistint_len - number of elements in a linked dlistint_t list.
+ * @h: header of dlistint_t
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len_list;
+
+	for (len_list = 0; h != NULL; len_list++)
+		h = h->next;
+	return (len_list);
 }
